6d88d676d98f5f1c8275a41bf148891cc241a76e
Alexandra Winsor <38674528+awinsor@users.noreply.github.com>
2021-05-10 07:35:59 -0600

    Delete test directory

--- test/controllers/pages_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class PagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get projects" do
-    get pages_projects_url
-    assert_response :success
-  end
-
-end
